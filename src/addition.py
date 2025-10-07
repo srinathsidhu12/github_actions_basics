d0701cdb4f39c664f85c8df92b7c0bbb03788dbb
srinathsidhu12 <139327360+srinathsidhu12@users.noreply.github.com>
2025-10-07 17:04:28 +0530

    Update of two numbers to addition of three numbers

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,8 @@
 # app.py
 # This is a test commit
-#..........
-def add(a, b):
-    return a + b
+def add(a, b, c):
+    return a + b + c
 
 def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(1, 2, 1) == 4
+    assert add(1, -1, 1) == 1
